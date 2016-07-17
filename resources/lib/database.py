1adb861bdb3328d73ad7f2e2b9035048effb5aa9
angelblue05 <tamara.angel05@gmail.com>
2016-07-17 18:17:58 -0500

    Update logging in database class
    Ensure we also use the built in sqlite3 context manager for the database
    connection.

--- resources/lib/database.py
+++ resources/lib/database.py
@@ -7,6 +7,8 @@ import threading
 
 import xbmc
 
+import utils
+
 #################################################################################################
 
 KODI = xbmc.getInfoLabel('System.BuildVersion')[:2]
@@ -51,22 +53,28 @@ class DatabaseConn(object):
     # To be called as context manager - with statement
 
     def __init__(self, database_file="video", commit_mode=None):
+        
         """
-        database_file can be custom. See self._SQL below
+        database_file can be custom: emby, texture, music, video, custom like :memory: or path
         commit_mode is set to None for isolation_level. See python documentation.
         """
+        global log
+        log = utils.Logging('Database').log
+
         self.db_file = database_file
         self.commit_mode = commit_mode
 
     def __enter__(self):
         # Open the connection
         self.path = self._SQL(self.db_file)
+        log("Opening database: %s" % self.path, 1)
 
         self.conn = sqlite3.connect(self.path, isolation_level=self.commit_mode, timeout=20)
-        return self.conn
+        with self.conn as conn:
+            return conn
 
     def _SQL(self, media_type):
-        # media_type: emby, texture, music, video, custom like :memory: or path
+
         if media_type == "emby":
             return xbmc.translatePath("special://database/emby.db").decode('utf-8')
         elif media_type == "texture":
@@ -83,12 +91,20 @@ class DatabaseConn(object):
         self.conn.close()
 
         if exc_type is not None:
-            # Errors were raised
-            # log("Error type %s value %s traceback %s" % (exc_type, exc_val, exc_tb))
-            pass
+            # Errors were raised in the with statement
+            log("Type: %s Value: %s Traceback: %s" % (exc_type, exc_val, exc_tb))
 
-def query(query, connection=None, conn_type=None, *args):
-    # For queries that don't return results
+def dbquery(query, connection=None, conn_type=None, *args):
+    
+    """
+    connection is sqlite.connect
+    conn_type is only required if a connection is not provided
+    *args are tuple values that contain the corresponding data for the query
+    i.e     args_executemany = (('value1', 'value2'),('value1', 'value2'))
+            args_execute = ('value1', 'value2')
+    tip: to build args_executemany, build a list of sets [(v1,v2),(v1,v2)], then pass it as *args
+    """
+    
     if connection is None:
         if conn_type is None:
             return False
